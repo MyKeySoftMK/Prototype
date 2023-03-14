6b397e00818f246f22f9c289a1de35234ff1b1ea
michael.kolowicz@mykeysoft.lan <michael.kolowicz@mykeysoft.lan>
2023-03-14 19:28:01 +0100

    Aufruf über WPF möglich

--- 01_WPF/WrkFlows/MainWindow.xaml.cs
+++ 01_WPF/WrkFlows/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,7 +23,15 @@ namespace WrkFlows
     {
         public MainWindow()
         {
+
+            var services1 = new ServiceCollection();
+            services1.AddWpfBlazorWebView();
+            services1.AddBlazorWebViewDeveloperTools();
+            var serviceProvider = services1.BuildServiceProvider();
+            Resources.Add("services1", serviceProvider);
+
             InitializeComponent();
+
         }
     }
 }
